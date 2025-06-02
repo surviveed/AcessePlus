61ef9c898e18356d5a91fc06482e6f55d62c7391
gabrielaPLMPRO <gabriela@plmpro.com.br>
2025-06-02 20:50:42 -0300

    adicionando carrossel

--- AcessePlus/Controllers/Site/HomeController.cs
+++ AcessePlus/Controllers/Site/HomeController.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using AcessePlus.Modelo;
+using AcessePlus.Models.ViewModels;
 
-namespace AcessePlus.Controllers;
-
-[Route("/")]
-public class HomeController : Controller
+namespace AcessePlus.Controllers.Site
 {
-    public async Task<IActionResult> Index()
+    [Route("/")]
+    public class HomeController : Controller
     {
-        return View();
+        public async Task<IActionResult> Index()
+        {
+            // ajustar p buscar do banco
+            var locais = new List<Local>
+            {
+                new Local { Id = 1, Nome = "Praça Central", Endereco = "Rua Principal, 123", Observacoes = "Possui rampa e sinalização tátil" },
+                new Local { Id = 2, Nome = "Biblioteca Municipal", Endereco = "Av. Leitura, 45", Observacoes = "Banheiro adaptado e elevador" },
+                new Local { Id = 3, Nome = "Teatro da Cidade", Endereco = "Rua das Artes, 78", Observacoes = "Acesso com guia rebaixada e assentos reservados" }
+            };
+
+            var vm = new HomeViewModel
+            {
+                Locais = locais
+            };
+
+            return View(vm);
+        }
     }
 }
---
+++ AcessePlus/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using AcessePlus.Modelo;
+
+namespace AcessePlus.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Local> Locais { get; set; }
+    }
+}
